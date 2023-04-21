c35ac3c6d53d6d0ff9b4526194c2796868b73dbd
NorthernWilly <ianwilson2791@gmail.com>
2023-04-21 16:45:45 +0100

    first commit

--- src/database.py
+++ src/database.py
@@ -1,4 +1,7 @@
 import csv
+from reminder import PoliteReminder 
+from deadlined_reminders import DateReminder
+
 
 def list_reminders():
     f = open("reminders.csv", "r")
@@ -12,8 +15,8 @@ def list_reminders():
                 print(e.ljust(32), end=' ')
         print()
 
-def add_reminder(text):
-
+def add_reminder(text, date):
+    reminder = DateReminder(text, date)
     with open('reminders.csv', 'a+', newline='\n') as file:
         writer = csv.writer(file)
-        writer.writerow([text])
+        writer.writerow(reminder)
